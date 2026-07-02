540d9fecf47fca2f173b6c03a3adaeb5a5cc43c3
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2026-07-02 12:15:19 +0800

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	log.Println("Go running...")
+
+	// load .env
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment")
+	}
+
+	// store env vars
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	appPort := os.Getenv("APP_PORT")
+
+	// build DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	conn, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// wiring chat
+	// repo := db.NewUserMySQL(conn)
+	// service := app.NewUserService(repo)
+
+	r := gin.Default()
+	// userhttp.NewUserHandler(r, service)
+
+	log.Printf("API running on http://localhost:%s\n", appPort)
+	r.Run(":" + appPort)
+}
